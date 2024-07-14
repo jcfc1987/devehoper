a88b2144e139257aa923b766f2179b5d968cda5e
jcfc1987 <carloscorreia.87@gmail.com>
2024-07-14 23:23:24 +0100

    fixed top header mobile; added more ln;

--- js/translations.js
+++ js/translations.js
@@ -5,7 +5,9 @@ const TRANSLATIONS = {
         "ln_services" : "Serviços",
         "ln_portfolio" : "Portfolio",
         "ln_contact" : "Contacto",
-        "ln_software_development": "Desenvolvimento de Software"
+        "ln_software_development": "Desenvolvimento de Software",
+        "ln_software_description": "Concebemos, projetamos, codificamos, testamos e lançamos software.",
+        "ln_details": "Detalhes"
     },
     "en": {
         "ln_home" : "Home",
@@ -13,6 +15,8 @@ const TRANSLATIONS = {
         "ln_services" : "Services",
         "ln_portfolio" : "Portfolio",
         "ln_contact" : "Contact",
-        "ln_software_development": "Software Development"
+        "ln_software_development": "Software Development",
+        "ln_software_description": "We conceive, design, code, test, and release software",
+        "ln_details": "Details"
     }
 };
