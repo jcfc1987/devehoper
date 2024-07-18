1ac20bfb9f6d75a9e953cba0729ff352a41536c0
jcfc1987 <carloscorreia.87@gmail.com>
2024-07-18 14:25:38 +0100

    Added budget service

--- js/translations.js
+++ js/translations.js
@@ -7,7 +7,10 @@ const TRANSLATIONS = {
         "ln_contact" : "Contacto",
         "ln_software_development": "Desenvolvimento de Software",
         "ln_software_description": "Concebemos, projetamos, codificamos, testamos e lançamos software.",
-        "ln_details": "Detalhes"
+        "ln_details": "Detalhes",
+        "ln_computer_repair": "Reparação de computadores",
+        "ln_budget_service": "Por favor contacte-nos para um serviço de orçamento gratuito",
+        "ln_budget": "Orçamentos"
     },
     "en": {
         "ln_home" : "Home",
@@ -17,6 +20,9 @@ const TRANSLATIONS = {
         "ln_contact" : "Contact",
         "ln_software_development": "Software Development",
         "ln_software_description": "We conceive, design, code, test, and release software",
-        "ln_details": "Details"
+        "ln_details": "Details",
+        "ln_computer_repair": "Computer Repair",
+        "ln_budget_service": "Please contact us for a free budget service",
+        "ln_budget": "Budgets"
     }
 };
